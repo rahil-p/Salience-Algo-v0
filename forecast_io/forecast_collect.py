9315885d0c2e07e67614104377dd552d5a6bd47a
rahil-p <rmp2177@gmail.com>
2018-08-25 01:31:38 -0400

    early commit

--- forecast_io/forecast_collect.py
+++ forecast_io/forecast_collect.py
@@ -7,16 +7,5 @@ ts = TimeSeries(key=config.api_key)
 
 #-----
 def get_stock_intraday(symbol):
-    data, meta_data = ts.get_intraday(symbol, interval='1min')
+    data, meta_data = ts.get_intraday(symbol, interval='1min', outputsize='full')
     return data, meta_data
-
-
-
-
-#-----
-def main():
-    data, _ = get_stock_intraday('ENPH')
-    print(data)
-
-if __name__ == '__main__':
-    main()
--- sa.py
+++ sa.py
@@ -1,6 +1,7 @@
 from robinhood_io.rh_navigate import rh_login, test_login, rh_open
 from robinhood_io.rh_debug import debug_mode, continue_mode                 #remove any if obsolete
 from stock_io.stock_mgmt import stock_requests                              #uses Stock class
+from forecast_io.forecast_collect import get_stock_intraday
 from selenium import webdriver
 
 import os
@@ -18,11 +19,12 @@ def entry(driver):
 def main():
     driver = webdriver.Chrome(os.getcwd() + '/robinhood_io/chromedriver')
 
-    entry(driver)
+    entry(driver)                                                                   #login and test login
 
-    sa_requests = stock_requests()
-    rh_open(driver, sa_requests)
-
+    sa_requests = stock_requests()                                                  #request and store from user inputs
+    rh_open(driver, sa_requests)                                                    #open each stock in a new tab
+    sa_data = {req.symbol:get_stock_intraday(req.symbol) for req in sa_requests}    #collect intraday data into a dict
+    
 
 if __name__ == '__main__':
     main()
